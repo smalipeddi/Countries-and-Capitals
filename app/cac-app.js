f36a4d7f9bafab3eb18238c9343a4b50e385a23d
smalipeddi <sunitha_malipeddi@yahoo.com>
2015-07-17 21:32:05 -0400

    new changes

--- app/cac-app.js
+++ app/cac-app.js
@@ -1,68 +1,75 @@
-var myapp = angular.module('cacApp', ['ngRoute', 'ngAnimate']);
-myapp.config(function($locationProvider, $routeProvider) {
-  /*$locationProvider.hashPrefix('!');*/
-  $routeProvider.when('/',{
-    templateUrl:'./views/home.html'
+  var myapp = angular.module('cacApp', ['ngRoute', 'ngAnimate']);
+  myapp.config(function($locationProvider, $routeProvider) {
+    /*$locationProvider.hashPrefix('!');*/
+    $routeProvider.when('/', {
+      templateUrl: './views/home.html'
 
-  });
-  $routeProvider.when('/countries',{
-   templateUrl:'./views/country.html',
-   controller :'countryCtrl'
- });
-  $routeProvider.when('/countries/:country/:capital',{
-    templateUrl:'./views/capitals.html',
-    controller :'capitalCtrl'
-  });
-  
-}).factory("countryData", function($http) {
-  return function() {
-    return $http.get('http://api.geonames.org/countryInfoJSON?username=smekala');
-  };
-}).factory("capitalsData", function($http) {
-  return function() {
-  
-        var url = "http://api.geonames.org/search?";
-        var request = {
-            username : 'smekala',
-            q :'bellary',
-            name:'India'
-        };
-      return $http.post('http://api.geonames.org/searchJSON?username=smekala&q=bellary&name=bellary&isNameRequired')
+    });
+    $routeProvider.when('/countries', {
+      templateUrl: './views/country.html',
+      controller: 'countryCtrl'
+    });
+    $routeProvider.when('/countries/:country/:capital', {
+      templateUrl: './views/capitals.html',
+      controller: 'capitalCtrl'
+    });
 
-};
- })
-.controller('countryCtrl', ['$scope', 'countryData', function($scope, countryData) {
- countryData().success(function(data) {
-   var jsonData =data;
-   /*console.log(jsonData.geonames);*/
-   $scope.result = jsonData.geonames;
-   console.log(typeof(jsonData.geonames));
- })
-}]).controller('capitalCtrl',['$scope','capitalsData', '$routeParams' ,function($scope,capitalsData ,$routeParams){
- capitalsData().success(function(data){
-  var jsonData = data.geonames;
-  $scope.capitalData = jsonData;
-  console.log( $scope.capitalData);;
-})
+  }).factory("countryData", function($http) {
+    return function() {
+      return $http.get('http://api.geonames.org/countryInfoJSON?username=smekala');
+    };
+  })
+  .controller('countryCtrl', ['$scope', 'countryData', function($scope, countryData) {
+    countryData().success(function(data) {
+      var jsonData = data;
+       $scope.result = jsonData.geonames;
+      })
+  }]).controller('capitalCtrl', ['$scope','$http','$routeParams', 'countryData', function($scope,$http, countryData,$routeParams) 
+  {
+    $http.get('http://api.geonames.org/countryInfoJSON?username=smekala').success(function(data){
+      var jsonData = data;
+      console.log("ssunitha");
+      $scope.result = jsonData.geonames;
 
- $scope.country = $routeParams.name;
- console.log($scope.country);
-}]);
+      /* console.log(typeof(jsonData.geonames));*/
+      for (var key in $scope.result)
+      {
+        if ($scope.result.hasOwnProperty(key))
+        {
+          // here you have access to
+          var cName = $scope.result[key].countryCode;
+          var cName =  encodeURIComponent(cName);
+          /*  console.log(cName);*/
+          var capital = $scope.result[key].capital;
+          var capital = encodeURIComponent(capital);
+          var population = $scope.result[key].population;
+          var population = encodeURIComponent(population);
+          var area = $scope.result[key].areaInSqKm;
+          var area = encodeURIComponent.areaInSqKm;
+          /* console.log(capital);*/
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
+        var username = "smekala";
+        $http.post('http://api.geonames.org/searchJSON?username='+username+'&name='+capital+'&country='+cName)
+        .success(function(data, status, headers, config) {
+          console.log('Success!');
+          console.log(data);
+          $scope.capitalData = data.geonames;
+        })
+        .error(function(data, status, headers, config) {
+          console.log('Failure :(');
+        });
 
 
 
+        
 
+      }
+
+
+
+    })
+
+}
+]);
 
